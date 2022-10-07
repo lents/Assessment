b40c6342ede826bf7645ce55007942e087b8dd3f
Elena Sycheva <Elena_Tsygankova@hotmail.com>
2022-10-07 13:12:21 +0300

    add validation

--- Assessment.Core/Testlet.cs
+++ Assessment.Core/Testlet.cs
@@ -1,23 +1,29 @@
-﻿using System.Runtime.ConstrainedExecution;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Assessment.Core
 {
     public class Testlet
     {
+        public const int NUMBER_OF_PRETEST_ITEMS = 4;
+        public const int NUMBER_OF_FIRST_PRETEST_ITEMS = 2;
+        public const int NUMBER_OF_OPERATIONAL_ITEMS = 6;
+
         public string TestletId;
         private List<Item> Items;
         public Testlet(string testletId, List<Item> items)
         {
             TestletId = testletId;
             Items = items;
-        }
+            ValidateItems();
+        }        
+
         public List<Item> Randomize()
         {
             var random = new Random();
             Items = Items.OrderBy(item => random.Next()).ToList();
 
             var pretestItems = Items.Where(i => i.ItemType == ItemTypeEnum.Pretest).ToList();
-            var randomizedPretestItems = pretestItems.OrderBy(item => random.Next()).Take(2);            
+            var randomizedPretestItems = pretestItems.OrderBy(item => random.Next()).Take(NUMBER_OF_FIRST_PRETEST_ITEMS);            
 
             foreach (var pretestItem in randomizedPretestItems)
             {
@@ -25,6 +31,22 @@ namespace Assessment.Core
                 Items.Insert(0, pretestItem);                
             }            
             return Items;
+        }
+
+        private void ValidateItems()
+        {
+            var pretestItemsCount = Items.Count(i => i.ItemType == ItemTypeEnum.Pretest);
+            if (pretestItemsCount != NUMBER_OF_PRETEST_ITEMS)
+            {
+                throw new ValidationException($"Testlet should have {NUMBER_OF_PRETEST_ITEMS} pretest items");
+            }
+
+            var operationalItemsCount = Items.Count(i => i.ItemType == ItemTypeEnum.Operational);
+            if (operationalItemsCount != NUMBER_OF_OPERATIONAL_ITEMS)
+            {
+                throw new ValidationException($"Testlet should have {NUMBER_OF_OPERATIONAL_ITEMS} operational items");
+            }
+
         }
     }
 }
--- Assessment.Test/TestletTest.cs
+++ Assessment.Test/TestletTest.cs
@@ -1,13 +1,28 @@
 using Assessment.Core;
+using System.ComponentModel.DataAnnotations;
 
 namespace Assessment.Test
 {
     public class TestletTest
     {
+        [Theory]
+        [InlineData(3)]
+        public void Should_Validate_Correct_Number_Pretest_Items(int numberOfPretest)
+        {
+            Assert.Throws<ValidationException>(() => { var testlet = CreateTestlet(numberOfPretest, Testlet.NUMBER_OF_OPERATIONAL_ITEMS); });
+        }
+
+        [Theory]
+        [InlineData(5)]
+        public void Should_Validate_Correct_Number_Operational_Items(int numberOfOperational)
+        {
+            Assert.Throws<ValidationException>(() => { var testlet = CreateTestlet(Testlet.NUMBER_OF_PRETEST_ITEMS, numberOfOperational); });
+        }
+
         [Fact]
         public void Should_Have_First_Two_Items_Pretest()
         {
-            var testlet = CreateTestletWithTenItems();
+            var testlet = CreateTestlet();
 
             var items = testlet.Randomize();
 
@@ -17,7 +32,7 @@ namespace Assessment.Test
         [Fact]
         public void Should_Have_All_Items_Random()
         {
-            var testlet = CreateTestletWithTenItems();
+            var testlet = CreateTestlet();
 
             var items = testlet.Randomize();
             var randomizedItems = testlet.Randomize();
@@ -39,22 +54,18 @@ namespace Assessment.Test
             });
         }
 
-        private Testlet CreateTestletWithTenItems()
+        private Testlet CreateTestlet(int numberOfPretestItems = Testlet.NUMBER_OF_PRETEST_ITEMS, int numberOfOperationalItems = Testlet.NUMBER_OF_OPERATIONAL_ITEMS)
         {
-            return new Testlet(Guid.NewGuid().ToString(),
-                new List<Item>
-                {
-                    new Item{ItemId = "Operational_1", ItemType = ItemTypeEnum.Operational},
-                    new Item{ItemId = "Operational_2", ItemType = ItemTypeEnum.Operational},
-                    new Item{ItemId = "Operational_3", ItemType = ItemTypeEnum.Operational},
-                    new Item{ItemId = "Operational_4", ItemType = ItemTypeEnum.Operational},
-                    new Item{ItemId = "Operational_5", ItemType = ItemTypeEnum.Operational},
-                    new Item{ItemId = "Operational_6", ItemType = ItemTypeEnum.Operational},
-                    new Item{ItemId = "Pretest_1", ItemType = ItemTypeEnum.Pretest},
-                    new Item{ItemId = "Pretest_2", ItemType = ItemTypeEnum.Pretest},
-                    new Item{ItemId = "Pretest_3", ItemType = ItemTypeEnum.Pretest},
-                    new Item{ItemId = "Pretest_4", ItemType = ItemTypeEnum.Pretest}
-                });
+            var items = new List<Item>();
+            for (int i = 0; i < numberOfOperationalItems; i++)
+            {
+                items.Add(new Item { ItemId = i.ToString(), ItemType = ItemTypeEnum.Operational });
+            }
+            for (int i = 0; i < numberOfPretestItems; i++)
+            {
+                items.Add(new Item { ItemId = i.ToString(), ItemType = ItemTypeEnum.Pretest });
+            }
+            return new Testlet(Guid.NewGuid().ToString(), items);
         }
     }
 }
